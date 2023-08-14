db4db76354c59f259828cbfd4e1a1e44dfce3b50
haipv01 <haipv@bsscommerce.com>
2023-08-14 23:12:09 +0700

    custom toast message animation

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+LogBox.ignoreAllLogs()
+
